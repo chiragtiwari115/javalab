29d03a78048dec33f2750a5b68b231b242aabc12
chiragtiwari115 <144206934+chiragtiwari115@users.noreply.github.com>
2023-09-10 00:36:23 +0530

    Add files via upload

---
+++ question6.java
@@ -0,0 +1,17 @@
+//chirag tiwari
+//0827CI221047
+import java.util.Scanner;
+public class question6 {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int a;
+        System.out.println("enter a numeric value");
+        a = in.nextInt();
+        if (a>0){
+            System.out.println("the entered numeric is positive");
+        }else{
+            System.out.println("the entered numeric is negative");
+        }
+
+    }
+}
